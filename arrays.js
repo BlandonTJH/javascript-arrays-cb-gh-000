c09a777b1d7dcc3fa56cee967f8550fec9a31244
Blandon Tan <e170095@e.ntu.edu.sg>
2020-05-30 04:27:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray(){
- chocolateBars = ["foo",...chocolateBars]
+ chocolateBars = ['foo',...chocolateBars]
 }
 
 function destructivelyAddElementToBeginningOfArray(){
